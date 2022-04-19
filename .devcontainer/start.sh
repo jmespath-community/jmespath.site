98707bb2e564e35ff69e33ea3e45a5ec57f6cef0
springcomp <springcomp@users.noreply.github.com>
2022-04-19 21:44:21 +0200

    Supports devcontainer.

---
+++ .devcontainer/start.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+## This devcontainer is mounting the workspace
+## in the /src folder which gets its original contents ignored.
+## We need to recreate the symlink to /usr/local/bin/jp
+
+if [[ ! -f "/src/jp" ]]; then
+  ln -s /usr/local/bin/jp /src/jp
+fi
+
+## The static website pulls most of its contents
+## from the jmespath.spec site and corresponding wiki.
+## Clone those repositories here as a courtesy
+
+if [[ ! -d "/src/content/spec" ]]; then
+  git clone --depth 1 https://github.com/jmespath-community/jmespath.spec.git /src/content/spec/
+fi
+if [[ ! -d "/src/content/wiki" ]]; then
+  git clone --depth 1 https://github.com/jmespath-community/jmespath.spec.wiki.git /src/content/wiki/
+fi
+
+## This repository uses symlinks
+## If you are starting this devcontainer from Windows
+## we fix this transparently here
+
+rm /src/data/functions
+ln -s /src/content/spec/functions /src/data/functions
+
+## Start local server to process JMESPath expressions
+## This is required during static site genration and
+## to power the live samples from the website.
+## It can be called with a simple curl command:
+## curl -X POST --location http://localhost:8000/json.txt  \
+## -H 'Content-Type: text/plain' \
+## --data-binary @- << EOF
+## foo
+## {"foo": "bar"}
+## EOF
+##
+
+ncat -lk -p 8000 -e ".github/workflows/jp_service.sh" &
+
+## Serve static web site
+
+hugo server
